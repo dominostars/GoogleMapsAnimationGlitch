d0a12e8082b88504420faf1574dc2de2624f92f5
Gilad Gurantz <gilad@lyft.com>
2015-08-08 09:02:10 -0700

    Initial Commit

---
+++ GoogleMapsAnimationGlitch/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GoogleMapsAnimationGlitch
+//
+//  Created by Gilad Gurantz on 8/8/15.
+//  Copyright (c) 2015 Lazy Arcade. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
